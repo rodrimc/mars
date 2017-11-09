bd90ffbc19310ecad4e3612192bf97ed8250983a
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-11-09 18:52:54 -0200

    Add tv_v2.ceu sample
    * src/scripts/tv_mappings_v2.lua
    * src/tv_v2.ceu

---
+++ src/scripts/tv_mappings_v2.lua
@@ -0,0 +1,10 @@
+CLIENT.mapping.events =
+  {
+    {"SPACE", "CONTROL",  "PLAY", "TV"},
+    {"P", "CONTROL",  "PAUSE", "TV"},
+    {"RIGHT", "CONTROL",  "SEEK_FORWARD", "TV"},
+    {"LEFT", "CONTROL",  "SEEK_BACKWARD", "TV"},
+    {"ESC", "CONTROL",  "TURN_OFF", "TV"},
+  }
+
+--CLIENT.mapping.apply = apply
