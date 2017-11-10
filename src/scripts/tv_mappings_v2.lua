df40a4e3ad529fa7e3d79c5f1c37ba98677a11f9
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-11-10 00:06:34 -0200

    Improve tv_v2.ceu example
    * resources/vol_0.png
    * resources/vol_1.png
    * resources/vol_2.png
    * resources/vol_3.png
    * resources/vol_4.png
    * resources/vol_5.png
    * src/scripts/tv_mappings_v2.lua
    * src/tv_v2.ceu

--- src/scripts/tv_mappings_v2.lua
+++ src/scripts/tv_mappings_v2.lua
@@ -1,10 +1,13 @@
 CLIENT.mapping.events =
   {
-    {"SPACE", "CONTROL",  "PLAY", "TV"},
-    {"P", "CONTROL",  "PAUSE", "TV"},
-    {"RIGHT", "CONTROL",  "SEEK_FORWARD", "TV"},
-    {"LEFT", "CONTROL",  "SEEK_BACKWARD", "TV"},
-    {"ESC", "CONTROL",  "TURN_OFF", "TV"},
+    {"SPACE", "CONTROL", "PLAY", "TV"},
+    {"P", "CONTROL", "PAUSE", "TV"},
+    {"RIGHT", "CONTROL", "SEEK_FORWARD", "TV"},
+    {"LEFT", "CONTROL", "SEEK_BACKWARD", "TV"},
+    {"UP", "CONTROL", "VOLUME_UP", "TV"},
+    {"DOWN", "CONTROL", "VOLUME_DOWN", "TV"},
+    {"RETURN_", "CONTROL", "TURN_ON", "TV"},
+    {"ESC", "CONTROL", "TURN_OFF", "TV"},
   }
 
 --CLIENT.mapping.apply = apply
