3c850d8f5fdc7e7226df1ecbdd4f042cb6806078
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-11-11 12:18:02 -0200

    Finish heineken example
    * src/heineken/heineken.ceu
    * src/heineken/heineken_mappings.lua

--- src/heineken/heineken_mappings.lua
+++ src/heineken/heineken_mappings.lua
@@ -1,8 +1,9 @@
 CLIENT.mapping.events =
   {
-    {"QUESTION", "TV",  "SHOW_OPTIONS", "TABLET"},
     {"CHOICE", "TABLET",  "CHOSEN_OPTION", "TV"},
+    {"ACCEPT", "TABLET",  "ACCEPTED", "TV"},
     {"END_", "TV",  "KILL_APP", "TABLET"},
+    {"QUESTION", "TV",  "SHOW_OPTIONS", "TABLET"},
   }
 
 --CLIENT.mapping.apply = apply
