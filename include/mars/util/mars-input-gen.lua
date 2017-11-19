468323ef3d45ec77ec7ea58f93176156f4ce347a
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-11-19 12:18:26 -0200

    Generate code just for declared input events
    * include/mars/util/mars-input-gen.lua

--- include/mars/util/mars-input-gen.lua
+++ include/mars/util/mars-input-gen.lua
@@ -4,18 +4,46 @@ if not arg[1] then
   return
 end
 
+if not arg[2] then
+  print ('Error: no source file specified')
+  return
+end
+
 local f = io.open (arg[1], 'r')
 local defs = f:read ('*all')
 f:close()
 
+f = io.open (arg[2], 'r')
+local src = f:read ('*all')
+f:close()
+
+local srcinputs = {}
+for line in io.lines (arg[2]) do
+  line = line:gsub("^%s*(.-)%s*$", "%1")
+  if line:sub (1, 5) == 'input' then
+    local evt = line:match ("%).-$"):sub(2):gsub("^%s*(.-)%s*;$", "%1");
+    srcinputs[evt] = true
+  end
+end
+
+local HANDLER_TEMPLATE =
+'code/await Emit_Input_Event (var _char&& evt) -> none\n' ..
+'do\n'                                                    ..
+'{#1}'                                                    ..
+'end'
+
+local HANDLER_CHECK_TEMPLATE =
+'\tif _strcmp(evt,"{#1}") == 0 then\n'                    ..
+  '\t\t{#2}\n'                                            ..
+  '\t\tawait Emit_{#3}_Event ({#4});\n'                   ..
+'\tend\n'
+
 local CODE_TEMPLATE =
-'#ifdef _CEU_INPUT_{#0}\n'                          ..
-  'code/await Emit_{#1}_Event ({#2}) -> none\ndo\n' ..
-    '\tawait async ({#3}) do\n'                     ..
-    '\t\temit {#4} ({#5});\n'                       ..
-    '\tend\n'                                       ..
-  'end\n'                                           ..
-'#endif'
+  'code/await Emit_{#1}_Event ({#2}) -> none\ndo\n'       ..
+    '\tawait async ({#3}) do\n'                           ..
+    '\t\temit {#4} ({#5});\n'                             ..
+    '\tend\n'                                             ..
+  'end\n'
 
 function capitalize_first (str)
   return str:lower():gsub('_.', string.upper):gsub('^.', string.upper)
@@ -43,38 +71,52 @@ end
 
 function interface (T)
   local gen = ''
+  local checks = ''
 
   for k,v in pairs (T) do
     for _,intable in pairs (v.inputs) do
       for evt, args in pairs (intable) do
-        local code = CODE_TEMPLATE:gsub ('{#0}', evt)
-                                  :gsub ('{#1}', capitalize_first(evt))
-        local paramdecl = ''
-        for i=1, #args do
-          paramdecl = paramdecl .. decl_param (args[i], i) .. ','
+        if srcinputs[evt] == true then
+          local code = CODE_TEMPLATE:gsub ('{#0}', evt)
+                                    :gsub ('{#1}', capitalize_first(evt))
+          local varcheckdecl = ''
+          local paramdecl = ''
+          for i=1, #args do
+            paramdecl = paramdecl .. decl_param (args[i], i) .. ','
+            varcheckdecl = varcheckdecl .. decl_param (args[i], i) ..
+                            ' = [[ CLIENT.remote.args[' .. i .. '] ]];\n\t\t'
+          end
+
+          if paramdecl == '' then
+            paramdecl = 'none'
+          else
+            paramdecl = paramdecl:sub(1, -2)
+          end
+
+          code = code:gsub('{#2}', paramdecl)
+
+          local async_args = get_vars_from_params (paramdecl)
+          local emit_args  = get_emit_args_from_vars (async_args)
+          code = code:gsub('{#3}', async_args)
+                     :gsub('{#4}', evt:upper())
+                     :gsub('{#5}', emit_args)
+
+          gen = gen .. code .. '\n\n'
+
+          local check = HANDLER_CHECK_TEMPLATE:gsub ('{#1}', evt)
+                                              :gsub ('{#2}', varcheckdecl)
+                                              :gsub ('{#3}', capitalize_first(evt))
+                                              :gsub ('{#4}', emit_args)
+          checks = checks .. check .. '\n'
         end
-
-        if paramdecl == '' then
-          paramdecl = 'none'
-        else
-          paramdecl = paramdecl:sub(1, -2)
-        end
-
-        code = code:gsub('{#2}', paramdecl)
-
-        local async_args = get_vars_from_params (paramdecl)
-        local emit_args  = get_emit_args_from_vars (async_args)
-        code = code:gsub('{#3}', async_args)
-        :gsub('{#4}', evt:upper())
-        :gsub('{#5}', emit_args)
-
-        gen = gen .. code .. '\n\n'
       end
     end
   end
 
-  if arg[2] then
-    f = io.open (arg[2], 'w')
+  gen = gen .. HANDLER_TEMPLATE:gsub('{#1}', checks)
+
+  if arg[3] then
+    f = io.open (arg[3], 'w')
     f:write (gen)
     f:close()
   end
