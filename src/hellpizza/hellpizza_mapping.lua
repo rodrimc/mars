b219a9b75781f6a49988593a4210834417be15a9
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-11-20 00:36:43 -0200

    Fix hellpizza sample
    * src/hellpizza/hellpizza.ceu
    * src/hellpizza/hellpizza_mapping.lua

--- src/hellpizza/hellpizza_mapping.lua
+++ src/hellpizza/hellpizza_mapping.lua
@@ -1,5 +1,4 @@
-CLIENT.mapping.events=
-  {
-    {"INTERACTIVITY", "MAIN", "SHOW_OPTIONS", "SECONDARY"},
-    {"FINAL_", "SECONDARY", "USER_CHOICE", "MAIN"},
-  }
+mapping {
+  {"INTERACTIVITY", "MAIN", "SHOW_OPTIONS", "SECONDARY"},
+  {"FINAL_", "SECONDARY", "USER_CHOICE", "MAIN"},
+}
