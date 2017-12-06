ad1fd277c99dba2234632e515fe144687eba348e
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-12-06 00:16:11 -0200

    Add control_v2.ceu and tv_mappings_v2.lua
    * src/tv_samples/control_v2.ceu
    * src/tv_samples/tv_mappings_v2.lua

--- src/tv_samples/tv_mappings_v2.lua
+++ src/tv_samples/tv_mappings_v2.lua
@@ -1,10 +1,46 @@
-mapping {
-  {"SPACE", "CONTROL_V1", "PLAY", "TV"},
-  {"P", "CONTROL_V1", "PAUSE", "TV"},
-  {"RIGHT", "CONTROL_V1", "SEEK_FORWARD", "TV"},
-  {"LEFT", "CONTROL_V1", "SEEK_BACKWARD", "TV"},
-  {"UP", "CONTROL_V1", "VOLUME_UP", "TV"},
-  {"DOWN", "CONTROL_V1", "VOLUME_DOWN", "TV"},
-  {"RETURN_", "CONTROL_V1", "TURN_ON", "TV"},
-  {"ESC", "CONTROL_V1", "TURN_OFF", "TV"},
-}
+tvs = {}
+controls = {}
+
+function link_control_tv (control, tv)
+  map (control, 'KEY_PRESSED', tv, 'TURN_ON',       function (_, key) return key == 0 end)
+  map (control, 'KEY_PRESSED', tv, 'PLAY',          function (_, key) return key == 1 end)
+  map (control, 'KEY_PRESSED', tv, 'PAUSE',         function (_, key) return key == 2 end)
+  map (control, 'KEY_PRESSED', tv, 'SEEK_FORWARD',  function (_, key) return key == 3 end)
+  map (control, 'KEY_PRESSED', tv, 'SEEK_BACKWARD', function (_, key) return key == 4 end)
+  map (control, 'KEY_PRESSED', tv, 'VOLUME_UP',     function (_, key) return key == 5 end)
+  map (control, 'KEY_PRESSED', tv, 'VOLUME_DOWN',   function (_, key) return key == 6 end)
+  map (control, 'KEY_PRESSED', tv, 'TURN_OFF',      function (_, key) return key == 7 end)
+end
+
+
+MARS.onConnect = function (p)
+  local interfaces = p:getInterfaces()
+  local isControl = false
+  local isTv = false
+
+  print (serialize(p))
+
+  for i,_ in pairs (interfaces) do
+    print (i)
+    if i == 'TV' then
+      table.insert (tvs, p)
+      isTv = true
+    elseif i == 'CONTROL_V2' then
+      table.insert (controls, p)
+      isControl = true
+    end
+  end
+
+  if isControl then
+    for _,tv in ipairs(tvs) do
+      link_control_tv (p, tv)
+    end
+  end
+
+  if isTv then
+    for _,control in ipairs (controls) do
+      link_control_tv (control, p)
+    end
+  end
+end
+
