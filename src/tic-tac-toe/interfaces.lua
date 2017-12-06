aa802b8bbec28aa39d7de75de624f29f5e1a2611
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-12-06 01:29:22 -0200

    Port tic-tac-toe sample
    * src/tic-tac-toe/interfaces.lua
    * src/tic-tac-toe/tic-tac-toe-mappings_v1.lua
    * src/tic-tac-toe/tic-tac-toe.ceu

---
+++ src/tic-tac-toe/interfaces.lua
@@ -0,0 +1,10 @@
+interfaces {
+  PLAYER = { 
+    inputs = {
+      OPPONENT_MOVE = {'int', 'int'}
+    },
+    outputs = {
+      MY_MOVE = {'int', 'int'},
+    },
+  },
+}
--- src/tic-tac-toe/tic-tac-toe-mappings_v1.lua
+++ src/tic-tac-toe/tic-tac-toe-mappings_v1.lua
@@ -1,5 +1,24 @@
-CLIENT.mapping.events =
-  {
-    {"MY_MOVE", "x",  "OPPONENT_MOVE", "o"},
-    {"MY_MOVE", "o",  "OPPONENT_MOVE", "x"},
-  }
+local previous_peer = nil
+
+MARS.onConnect = function (p)
+  local interfaces = p:getInterfaces()
+  local isPlayer = false
+
+  for i, _ in pairs (interfaces) do
+    if i == 'PLAYER' then
+      isPlayer = true
+      break
+    end
+  end
+
+  if isPlayer then
+    if not previous_peer then
+      previous_peer = p
+    else
+      map (p, 'MY_MOVE', previous_peer, 'OPPONENT_MOVE')
+      map (previous_peer, 'MY_MOVE', p, 'OPPONENT_MOVE')
+
+      previous_peer = nil
+    end
+  end
+end
