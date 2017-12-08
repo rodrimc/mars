d410199781a3148dceb7cbafea742a5b79d1c62e
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-12-07 21:11:32 -0300

    Fix heineken sample
    deleted:    src/heineken/heineken.ceu
    deleted:    src/heineken/interface.lua
    * src/heineken/heineken_main.ceu
    * src/heineken/heineken_mappings.lua
    * src/heineken/heineken_sec.ceu
    * src/heineken/interfaces.lua

--- src/heineken/heineken_mappings.lua
+++ src/heineken/heineken_mappings.lua
@@ -1,7 +1,41 @@
-mapping {
-  {"CHOICE", "TABLET",  "CHOSEN_OPTION", "TV"},
-  {"ACCEPT", "TABLET",  "ACCEPTED_", "TV"},
+tvs = {}
+tablets = {}
 
-  {"END_", "TV",  "KILL_APP", "TABLET"},
-  {"QUESTION", "TV",  "SHOW_OPTIONS", "TABLET"},
-}
+function link_peers (tv, tablet)
+  map (tv,     'END_',     tablet, 'KILL_APP')
+  map (tv,     'QUESTION', tablet, 'SHOW_OPTIONS')
+  map (tablet, 'CHOICE',   tv,     'CHOSEN_OPTION')
+  map (tablet, 'ACCEPT',   tv,     'ACCEPTED_')
+end
+
+MARS.onConnect = function (p)
+  local isTV, isTablet = false, false
+  local interfaces = p:getInterfaces()
+
+  for i,_ in pairs (interfaces) do
+    print (i)
+    print (i == 'TV')
+    print (i == 'TABLET')
+    if i == 'TV' then
+      table.insert (tvs, p)
+      isTV = true
+    elseif i == 'TABLET' then
+      table.insert (tablets, p)
+      isTablet = true
+    end
+  end
+
+  if isTV then
+    print ('isTV')
+    for _, tablet in ipairs (tablets) do
+      link_peers (p, tablet)
+    end
+  end
+
+  if isTablet then
+    print ('isTablet')
+    for _, tv in ipairs (tvs) do
+      link_peers (tv, p)
+    end
+  end
+end
