65d66ad77e39a6d3870a7f0874a7e577508b75a4
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-12-09 15:08:16 -0300

    Remove old files
    deleted:    src/scripts/classroom_mappings.lua
    deleted:    src/scripts/classroom_naming.lua
    deleted:    src/scripts/mappings.lua
    deleted:    src/scripts/videocrop_naming.lua

--- src/scripts/classroom_mappings.lua
+++
@@ -1,4 +0,0 @@
-CLIENT.evts.mapping = 
-  {
-    {"CM_SCENE_KEY", "STUDENT", "CONTROL_REQUEST"}
-  }
--- src/scripts/mappings.lua
+++
@@ -1,9 +0,0 @@
-setup = {
-  MIN_PEERS = 2,
-  ROUTE = {
-    {"CM_PLAYER_MOUSE_CLICK", 0, "A"},
-    {"CM_SCENE_MOUSE_CLICK", 0, "A"}
-  }
-}
-
-CLIENT.evts.mapping = setup.ROUTE
