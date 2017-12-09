93cc820a8e8887dcfd99d9893f2dfda2b6b94a08
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-12-09 12:13:35 -0300

    Fix hellpizza sample
    deleted:    src/hellpizza/hellpizza.ceu
    * src/hellpizza/hellpizza_main.ceu
    * src/hellpizza/hellpizza_mapping.lua
    * src/hellpizza/hellpizza_sec.ceu
    * src/hellpizza/interface.lua -> src/hellpizza/interfaces.lua

--- src/hellpizza/hellpizza_mapping.lua
+++ src/hellpizza/hellpizza_mapping.lua
@@ -1,4 +1,32 @@
-mapping {
-  {"INTERACTIVITY", "MAIN", "SHOW_OPTIONS", "SECONDARY"},
-  {"FINAL_", "SECONDARY", "USER_CHOICE", "MAIN"},
-}
+main = {}
+sec = {}
+
+MARS.onConnect = function (p)
+  local interfaces = p:getInterfaces()
+  local isMain = false 
+  local isSec = false
+  
+  for i in pairs (interfaces) do
+    if i == 'MAIN' then
+      isMain = true
+      table.insert (main, p)
+    elseif i == 'SECONDARY' then
+      isSec = true
+      table.insert (sec, p)
+    end
+  end
+
+  if isMain then
+    for _, sec in ipairs (sec) do
+      map (p,   'INTERACTIVITY', sec, 'SHOW_OPTIONS')
+      map (sec, 'FINAL_',        p,   'USER_CHOICE')
+    end
+  end
+
+  if isSec then
+    for _, main in ipairs (main) do
+      map (main, 'INTERACTIVITY', p,    'SHOW_OPTIONS')
+      map (p,    'FINAL_',        main, 'USER_CHOICE')
+    end
+  end
+end
