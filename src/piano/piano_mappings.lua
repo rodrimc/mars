e017fbfa79261267be1128471d1f1d9a9ad477fa
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-12-09 15:05:56 -0300

    Fix piano sample
    * src/piano/media.ceu
    * src/piano/piano.ceu -> src/piano/piano_keys.ceu
    * src/piano/piano_mappings.lua
    deleted:    src/piano/piano_naming.lua
    * src/piano/piano_sound.ceu

--- src/piano/piano_mappings.lua
+++ src/piano/piano_mappings.lua
@@ -1,4 +1,22 @@
-CLIENT.mapping.events =
-  {
-    {"NOTE", "TABLET",  "PLAY", "TV"},
-  }
+local sound = nil 
+local notes = {}
+
+MARS.onConnect = function (p)
+  local isSound = p:hasInterface ('SOUND') 
+  local isNote  = p:hasInterface ('NOTES') 
+
+  if isSound and not sound then
+    sound = p
+    for _, note in ipairs (notes) do
+      map (note, 'NOTE', p, 'PLAY')
+    end
+  end
+
+  if isNote then
+    if sound then
+      map (p, 'NOTE', sound, 'PLAY')
+    else
+      table.insert (notes, p)
+    end
+  end
+end
--- src/piano/piano_naming.lua
+++
@@ -1,9 +0,0 @@
-function naming (id) 
-  if id == 1 then
-    return 'TV' 
-  else
-    return 'TABLET'
-  end
-end
-
-SERVER.naming = naming 
