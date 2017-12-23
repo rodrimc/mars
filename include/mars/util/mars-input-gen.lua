9a03801d1802e258d5e5f2dfba0c5d67d9e02533
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2017-12-23 00:24:34 -0200

    Convert async await into calls to C functions
    * include/mars/util/mars-input-gen.lua
    * include/sync/maestro.ceu

--- include/mars/util/mars-input-gen.lua
+++ include/mars/util/mars-input-gen.lua
@@ -36,30 +36,34 @@ local HANDLER_TEMPLATE =
 local HANDLER_CHECK_TEMPLATE =
 '\tif _strcmp(&&evt[0],"{#1}") == 0 then\n'                ..
   '\t\t{#2}\n'                                             ..
-  '\t\tawait Emit_{#3}_Event ({#4});\n'                    ..
+  '\t\t_emit_{#3}_event ({#4});\n'                         ..
 '\tend\n'
 
 local CODE_TEMPLATE =
-  'code/await Emit_{#1}_Event ({#2}) -> none\ndo\n'        ..
-    '\tawait async ({#3}) do\n'                            ..
-    '\t\temit {#4} ({#5});\n'                              ..
-    '\tend\n'                                              ..
-  'end\n'
+  '\tvoid emit_{#1}_event ({#2})\n'                        ..
+  '\t{\n'                                                  ..
+  '\t\ttceu_input_{#3} p = {{#4}};\n'                      ..
+  '\t\tceu_input (CEU_INPUT_{#3}, &p);\n'                  ..
+  '\t}'
 
 function capitalize_first (str)
   return str:lower():gsub('_.', string.upper):gsub('^.', string.upper)
 end
 
 function decl_param (type_, index)
-  return 'var ' .. type_ .. ' param' .. index
+  return type_ .. ' param' .. index
 end
 
 function get_vars_from_params (params)
-  if params == 'none' then
-    return '_'
-  else
-    return params:gsub('var (.-) ', '')
+  local toReturn = ''
+  local tmp = 0
+  for token in params:gmatch ('(.-) ') do
+    tmp = tmp + 1
+    if tmp % 2 == 0 then
+      toReturn = toReturn .. token
+    end
   end
+  return toReturn
 end
 
 function get_emit_args_from_vars (args)
@@ -73,49 +77,52 @@ end
 function interfaces (T)
   local gen = ''
   local checks = ''
+  local decl_funcs = ''
 
   for name, t in pairs (T) do
     for evt, args in pairs (t.inputs) do
       if srcinputs[evt] == true then
-        local code = CODE_TEMPLATE:gsub ('{#0}', evt)
-                                  :gsub ('{#1}', capitalize_first(evt))
+        local code = CODE_TEMPLATE:gsub ('{#1}', evt:lower())
+        decl_funcs = decl_funcs .. '\t_emit_' .. evt:lower() .. '_event,\n'
+
         local varcheckdecl = ''
         local paramdecl = ''
         for i=1, #args do
-          paramdecl = paramdecl .. decl_param (args[i], i) .. ','
-          varcheckdecl = varcheckdecl .. decl_param (args[i], i) ..
-          ' = [[ MARS.message.args[' .. i .. '] ]];\n\t\t'
+          paramdecl = paramdecl .. decl_param (args[i], i) .. ', '
+          varcheckdecl = varcheckdecl .. 'var ' .. decl_param (args[i], i) ..
+            ' = [[ MARS.message.args[' .. i .. '] ]];\n\t\t'
         end
 
-        if paramdecl == '' then
-          paramdecl = 'none'
-        else
-          paramdecl = paramdecl:sub(1, -2)
+        if paramdecl ~= '' then
+          paramdecl = paramdecl:sub(1, -3) .. ' '
         end
+
+        local evt_args  = get_vars_from_params (paramdecl)
+        local emit_args = paramdecl
 
         code = code:gsub('{#2}', paramdecl)
-
-        local async_args = get_vars_from_params (paramdecl)
-        local emit_args  = get_emit_args_from_vars (async_args)
-        code = code:gsub('{#3}', async_args)
-                   :gsub('{#4}', evt:upper())
-                   :gsub('{#5}', emit_args)
+                   :gsub('{#3}', evt:upper())
+                   :gsub('{#4}', evt_args)
 
         gen = gen .. code .. '\n\n'
 
         local check = HANDLER_CHECK_TEMPLATE:gsub ('{#1}', evt)
                                             :gsub ('{#2}', varcheckdecl)
-                                            :gsub ('{#3}', capitalize_first(evt))
-                                            :gsub ('{#4}', emit_args)
+                                            :gsub ('{#3}', evt:lower())
+                                            :gsub ('{#4}', evt_args)
         checks = checks .. check .. '\n'
       end
     end
   end
 
-  gen = gen .. HANDLER_TEMPLATE:gsub('{#1}', checks)
+  if decl_funcs ~= '' then
+    decl_funcs = 'native/nohold\n' .. decl_funcs .. '\n;'
+  end
 
+  src = src:gsub ('__DECL_EMIT_FUNCTIONS__', decl_funcs)
+  src = src:gsub ('__EMIT_FUNCTIONS__', gen)
   src = src:gsub ('__INTERFACES__', defs:gsub ('interfaces', 'MARS.interfaces = '))
-  src = src:gsub ('__INPUTS__', gen)
+  src = src:gsub ('__INPUTS__', HANDLER_TEMPLATE:gsub('{#1}', checks))
 
   if arg[3] then
     f = io.open (arg[3], 'w')
